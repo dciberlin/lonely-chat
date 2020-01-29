50b61eb2d422e34f97901ffa895074d8dbfa42ca
Vasileios Psychas <vaspsych@gmail.com>
2020-01-29 13:43:36 +0100

    Connect our component to our store

--- src/components/MessagesContainer.js
+++ src/components/MessagesContainer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const MessagesContainer = props => {
-  return <h3>Messages</h3>;
+  return <h3>{props.message}</h3>;
 };
 
-export default MessagesContainer;
+const mapStateToProps = state => {
+  return state;
+};
+
+export default connect(mapStateToProps)(MessagesContainer);
