e95b7826d63b9dde8bd809a8e3777fbeda39252a
Vasileios Psychas <vaspsych@gmail.com>
2020-01-29 13:27:55 +0100

    The simplest redux configuration
    No actions at the moment

---
+++ src/components/FormContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormContainer = props => {
+  return <h3>FORM</h3>;
+};
+
+export default FormContainer;
---
+++ src/reducers/chatReducer.js
@@ -0,0 +1,11 @@
+// INITIAL STATE
+const initialState = {
+  message: 'Hello from other state'
+};
+
+// REDUCER
+const chatReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default chatReducer;
